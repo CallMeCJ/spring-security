cd416f5f99b35bb2011119c8f6367e61204c8193
CJZheng <jie081602@sina.com>
2018-01-11 18:57:08 +0800

    Initial commit

---
+++ src/main/java/com/cjzheng/Application.java
@@ -0,0 +1,18 @@
+package com.cjzheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by CJ Zheng on 2018/1/11.
+ * Project: spring-security
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/cjzheng/config/MvcConfig.java
@@ -0,0 +1,22 @@
+package com.cjzheng.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by CJ Zheng on 2018/1/11.
+ * Project: spring-security
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
